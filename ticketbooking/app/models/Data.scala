ca51d0c3a9b8d9f7a563c18767cf3d7fa0b4353e
Karol Wołonciej <kwolonciej@gmail.com>
2023-09-03 21:02:02 +0200

    commit

---
+++ ticketbooking/app/models/Data.scala
@@ -0,0 +1,14 @@
+package data
+
+object Data {
+  val moviesNames = Array("initial d stage one",
+                      "initial d stage two",
+                      "initial d stage three",
+                      "initial d extra stage",
+                      "initial d super stage",
+                      "initial d dark stage",
+                      "initial d train stage")
+}
+
+
+
---
+++ ticketbooking/app/models/test.scala
@@ -0,0 +1,21 @@
+import cinema.{Cinema}
+import scala.util.matching.Regex
+
+
+
+/*
+@main def main = {
+   
+  val r = "([a-zA-ZąĄęĘóÓćĆńŃłŁśŚźŹżŻ]{3,20}) ([a-zA-ZąĄęĘóÓćĆńŃłŁśŚźŹżŻ]{3,20})(-[a-zA-ZąĄęĘóÓćĆńŃłŁśŚźŹżŻ]{3,20}){0,1}".r 
+  val kino = new Cinema(9)
+
+  println(r.pattern.matcher("ManifolźĘaąd").matches)
+  //println(kino.getScreenings(12, 14, 15).toString)
+ 
+  val booking = List((0, List(("tosia", 1), ("zosia", 2), ("kasia", 3))))  
+  kino.makeReservation(1, 1, 1, booking) 
+  //println(kino.getFreePlaces(1, List((1, 1))))
+  //println(kino.isReservationValid(1, 1, 1, booking))
+
+}
+*/
